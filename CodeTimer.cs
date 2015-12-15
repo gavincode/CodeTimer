82e452bb47af46376c1f3e61425652bc8db66837
gavin <709181180@qq.com>
2015-12-15 16:14:40 +0800

    init project

---
+++ CodeTimer.cs
@@ -0,0 +1,99 @@
+﻿// ****************************************
+// FileName:CodeTimer.cs
+// Description:代码性能测试类
+// Tables:
+// Author:Gavin
+// Create Date:2015/1/14 15:09:03
+// Revision History:
+// ****************************************
+
+using System;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    /// <summary>
+    /// 代码运行时间测试类
+    /// </summary>
+    public static class CodeTimer
+    {
+        /// <summary>
+        /// 初始化(预热)
+        /// </summary>
+        public static void Initialize()
+        {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            Time("", 1, () => { });
+        }
+
+        /// <summary>
+        /// 时间计算
+        /// </summary>
+        /// <param name="name">测试名</param>
+        /// <param name="iteration">迭代次数</param>
+        /// <param name="action">执行的方法</param>
+        public static void Time(string name, int iteration, Action action)
+        {
+            if (String.IsNullOrEmpty(name)) return;
+
+            // 1.
+            ConsoleColor currentForeColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(name);
+
+            // 2.
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            int[] gcCounts = new int[GC.MaxGeneration + 1];
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                gcCounts[i] = GC.CollectionCount(i);
+            }
+
+            // 3.
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ulong cycleCount = GetCycleCount();
+            for (int i = 0; i < iteration; i++) action();
+            ulong cpuCycles = GetCycleCount() - cycleCount;
+            watch.Stop();
+
+            // 4.
+            Console.ForegroundColor = currentForeColor;
+            Console.WriteLine("\tTime Elapsed:\t" + watch.ElapsedMilliseconds.ToString("N0") + "ms");
+            Console.WriteLine("\tCPU Cycles:\t" + cpuCycles.ToString("N0"));
+
+            // 5.
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                int count = GC.CollectionCount(i) - gcCounts[i];
+                Console.WriteLine("\tGen " + i + ": \t\t" + count);
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 获取当前线程运行周期
+        /// </summary>
+        /// <returns></returns>
+        private static ulong GetCycleCount()
+        {
+            ulong cycleCount = 0;
+            QueryThreadCycleTime(GetCurrentThread(), ref cycleCount);
+            return cycleCount;
+        }
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool QueryThreadCycleTime(IntPtr threadHandle, ref ulong cycleTime);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentThread();
+    }
+}
